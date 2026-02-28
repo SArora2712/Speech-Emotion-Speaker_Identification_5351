627721576da34a0894d2d92263eb6e183487ba1f
unknown <sukhmanarora01@gmail.com>
2026-02-28 10:42:07 +0530

    Initial dataset integration: added RAVDESS dataset, implemented audio_analysis.py for waveform plotting and speech/song detection, and dataset analysis for counting .wav files

---
+++ dataset_analysis.py
@@ -0,0 +1,20 @@
+import os
+
+dataset_path="Ravdess"
+
+total_files=0
+speech_count=0
+song_count=0
+
+for root,dirs,files in os.walk(dataset_path):
+    for file in files:
+        if file.endswith(".wav"):
+            total_files+=1
+            if "speech" in root.lower():
+                speech_count+=1
+            elif "song" in root.lower():
+                song_count+=1
+
+print("Total .wav files: ",total_files)
+print("Speech Files: ",speech_count)
+print("Song Files: ",song_count)
